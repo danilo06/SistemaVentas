3485f58ad93c805ac08970e0ec8e25682ec5fa0b
JoseBuitrago <36308485+JoseBuitrago@users.noreply.github.com>
2018-11-15 12:02:38 -0500

    Update DbPropertiesReader.java

--- src/main/java/connection/DbPropertiesReader.java
+++ src/main/java/connection/DbPropertiesReader.java
@@ -2,7 +2,11 @@ package connection;
 
 import java.util.MissingResourceException;
 import java.util.Properties;
-
+/**
+ * Esta clase permite la conexion de de la base de datos
+ * @author Jose Buitrago, Danilo Beleno
+ *
+ */
 public class DbPropertiesReader {
 	
 	private static final String BUNDLE_NAME = "connection.properties";
@@ -11,7 +15,11 @@ public class DbPropertiesReader {
 
 	private DbPropertiesReader() {
 	}
-
+	/**
+	 * 
+	 * @param key
+	 * @return conxion realizada
+	 */
 	public static String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getProperty(key);
