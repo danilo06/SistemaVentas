e5a55c2253938a75fa2eb65383e15b352f2a78ad
JoseBuitrago <36308485+JoseBuitrago@users.noreply.github.com>
2018-11-15 15:03:54 -0500

    Update Producto.java

--- src/main/java/Modelo/Producto.java
+++ src/main/java/Modelo/Producto.java
@@ -43,51 +43,87 @@ public class Producto {
      * 
      */
     protected Double precio;
-
+	/**
+     	* 
+    	 * @return idProducto
+    	 */
 	public String getIdProducto() {
 		return idProducto;
 	}
-
+	/**
+	 * 
+	 * @param idProducto
+	 */
 	public void setIdProducto(String idProducto) {
 		this.idProducto = idProducto;
 	}
-
+	/**
+	 * 
+	 * @return nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-
+	/**
+	 * 
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	/**
+	 * 
+	 * @return marca
+	 */
 	public String getMarca() {
 		return marca;
 	}
-
+	/**
+	 * 
+	 * @param marca
+	 */
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-
+	/**
+	 * 
+	 * @return categoria
+	 */
 	public String getCategoria() {
 		return categoria;
 	}
-
+	/**
+	 * 
+	 * @param categoria
+	 */
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-
+	/**
+	 * 
+	 * @return contenido
+	 */
 	public String getContenido() {
 		return contenido;
 	}
-
+	/**
+	 * 
+	 * @param contenido
+	 */
 	public void setContenido(String contenido) {
 		this.contenido = contenido;
 	}
-
+	/**
+	 * 
+	 * @param precio
+	 */
 	public Double getPrecio() {
 		return precio;
 	}
-
+	/**
+	 * 
+	 * @param precio
+	 */
 	public void setPrecio(Double precio) {
 		this.precio = precio;
 	}
